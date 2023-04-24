133541e46350235da2df9214ff33b28030322c07
Sourav Kantha <souravkantha@Souravs-MacBook-Air.local>
2023-04-24 22:54:19 +0530

    test cases

---
+++ src/test/java/com/lru/impl/VanillaLRUCacheTest.java
@@ -0,0 +1,14 @@
+package com.lru.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class VanillaLRUCacheTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
